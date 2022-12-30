e30c8777f41ada5ab6ea8ea62f88548042d1419d
BigDramani1 <70725785+BigDramani1@users.noreply.github.com>
2022-12-30 00:30:03 +0000

    hh

---
+++ settings/db_cred.php
@@ -0,0 +1,14 @@
+<?php
+//Database credentials
+// define('SERVER', 'localhost');
+// define('USERNAME','root');
+// define('PASSWORD', '');
+// define('DATABASE', 'shoppn');
+
+// Database credentials for heroku
+define('SERVER', 'us-cdbr-east-06.cleardb.net');
+define('USERNAME','b2e6d7686e463f');
+define('PASSWORD', 'bc22ceae');
+define('DATABASE', 'heroku_63834e23f312c1a');
+?>
+
