330bc98fb9549ac9b20eaae4d5c560fff1cac30e
BigDramani1 <70725785+BigDramani1@users.noreply.github.com>
2023-01-09 09:59:19 +0000

    s

---
+++ classes/product_class.php
@@ -0,0 +1,10 @@
+<?php
+include_once(dirname(__FILE__)).'../../settings/connection.php';
+
+class Product extends Connection{
+    function inputing_user_info($name, $email, $city, $phone, $password, $code){
+        return $this->query("insert into customer(customer_name, customer_email, customer_city, customer_contact, customer_pass, code, status, user_role) values ('$name', '$email', '$city', '$phone','$password', '$code', 'notverified', 2)");
+    }
+
+
+}
--- settings/auction.sql
+++ settings/auction.sql
@@ -1,277 +1,261 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jun 13, 2022 at 02:42 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+drop database if exists auction;
+create database auction;
+use auction;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+create table customer(
+customer_id int not null auto_increment,
+fname varchar(30) not null,
+lname varchar(30) not null,
+country varchar(30) not null,
+address varchar (30) not null,
+password varchar(30) not null, 
+email varchar(30) unique not null,
+city varchar(30) not null,
+phone varchar (30) not null, 
+primary key (customer_id));
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `fashion`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `brands`
---
-
-CREATE TABLE `brands` (
-  `brand_id` int(11) NOT NULL,
-  `brand_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cart`
---
-
-CREATE TABLE `cart` (
-  `p_id` int(11) NOT NULL,
-  `ip_add` varchar(50) NOT NULL,
-  `c_id` int(11) DEFAULT NULL,
-  `qty` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
-
-CREATE TABLE `categories` (
-  `cat_id` int(11) NOT NULL,
-  `cat_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `customer`
---
-
-CREATE TABLE `customer` (
-  `customer_id` int(11) NOT NULL,
-  `customer_name` varchar(100) NOT NULL,
-  `customer_email` varchar(50) NOT NULL,
-  `customer_pass` varchar(150) NOT NULL,
-  `customer_city` varchar(30) NOT NULL,
-  `customer_contact` varchar(15) NOT NULL,
-  `status` text NOT NULL,
-  `user_role` int(11) NOT NULL,
-  `code` mediumint(9) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orderdetails`
---
-
-CREATE TABLE `orderdetails` (
-  `order_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `qty` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
+create table seller(
+seller_id int not null auto_increment,
+fname varchar(30) not null,
+lname varchar(30) not null,
+country varchar(30) not null,
+address varchar (30) not null,
+password varchar(30) not null, 
+email varchar(30) unique not null,
+city varchar(30) not null,
+phone varchar (30) not null, 
+primary key (seller_id));
 
 
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
-  `invoice_no` int(11) NOT NULL,
-  `order_date` date NOT NULL,
-  `order_status` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table seller_product(
+product_id int not null auto_increment,
+categories enum("SPORTS", "HOUSES", "WATCHES", "VEHICLES", "JEWELRY", "ELECTRONICS", "ARTS", "CARS"),
+image_id int not null unique,
+primary key(product_id));
 
--- --------------------------------------------------------
+create table sports(
+sports_id int not null auto_increment,
+product_id int not null,
+sport_type enum ("Gym Equipments", "Video Games", "Consoles", "Basketball", "Football", "Training Wear"),
+primary key(sports_id),
+foreign key (product_id) references seller_product (product_id));
 
---
--- Table structure for table `payment`
---
+create table sport_football(
+football_id int not null auto_increment,
+gender enum ("Male", "Female", "Unisex"),
+size enum ("Small","Medium", "Large"),
+sports_id int not null,
+Type enum ("Shoes", "Jerseys", "Socks","Balls"),
+brand varchar (30),
+image1 varchar(255),
+image2 varchar(255),
+image3 varchar(255),
+image4 varchar(255),
+title varchar (20),
+image_id int not null,
+minimum_bid_price int not null,
+buy_price int not null,
+conditions enum ("Brand New", "Ghanaian Used", "Foreign Used"),
+location enum("ACCRA", "KUMASI", "CAPE COAST", "TAKORADI", "TEMALE", "UPPER EAST", "UPPER WEST", "VOLTA REGION", "KOFORIDUA", "WA", "BOLGATANGA"),
+address varchar(30),
+description varchar(200),
+color varchar(20),
+primary key(football_id),
+foreign key (sports_id) references sports(sports_id),
+foreign key (image_id) references seller_product (image_id));
 
-CREATE TABLE `payment` (
-  `pay_id` int(11) NOT NULL,
-  `amt` double NOT NULL,
-  `customer_id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `currency` text NOT NULL,
-  `payment_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table sport_weight(
+weight_id int not null auto_increment,
+sports_id int not null,
+weight int not null, 
+brand enum ("Bodygrip", "Strauss", "Cockatoo","Aurion", "AmazonBasics", "Bowflex","SPRI", "Cap Barbell"),
+image1 varchar(255),
+image2 varchar(255),
+image3 varchar(255),
+title varchar (20),
+image4 varchar(255),
+image_id int not null,
+minimum_bid_price int not null,
+buy_price int not null,
+conditions enum ("Brand New", "Ghanaian Used", "Foreign Used"),
+location enum("ACCRA", "KUMASI", "CAPE COAST", "TAKORADI", "TEMALE", "UPPER EAST", "UPPER WEST", "VOLTA REGION", "KOFORIDUA", "WA", "BOLGATANGA"),
+address varchar(30),
+description varchar(200),
+color varchar(20),
+primary key(weight_id),
+foreign key (sports_id) references sports(sports_id),
+foreign key (image_id) references seller_product (image_id));
 
--- --------------------------------------------------------
+create table sport_consoles(
+console_id int not null auto_increment,
+sports_id int not null,
+brand enum ("PS4", "PS5", "Xbox One","Xbox 360", "Xbox Series X"),
+image1 varchar(255),
+image2 varchar(255),
+image3 varchar(255),
+image4 varchar(255),
+title varchar (20),
+image_id int not null,
+minimum_bid_price int not null,
+buy_price int not null,
+conditions enum ("Brand New", "Ghanaian Used", "Foreign Used"),
+location enum("ACCRA", "KUMASI", "CAPE COAST", "TAKORADI", "TEMALE", "UPPER EAST", "UPPER WEST", "VOLTA REGION", "KOFORIDUA", "WA", "BOLGATANGA"),
+address varchar(30),
+description varchar(200),
+color varchar(20),
+primary key(console_id),
+foreign key (sports_id) references sports(sports_id),
+foreign key (image_id) references seller_product (image_id));
 
---
--- Table structure for table `products`
---
+create table sport_games(
+game_id int not null auto_increment,
+sports_id int not null,
+Type enum ("PS4", "PS5", "Xbox One","Xbox 360", "Xbox Series X"),
+Game varchar (30),
+image1 varchar(255),
+image2 varchar(255),
+image3 varchar(255),
+image4 varchar(255),
+image_id int not null,
+title varchar (20),
+minimum_bid_price int not null,
+buy_price int not null,
+conditions enum ("Brand New", "Ghanaian Used", "Foreign Used"),
+location enum("ACCRA", "KUMASI", "CAPE COAST", "TAKORADI", "TEMALE", "UPPER EAST", "UPPER WEST", "VOLTA REGION", "KOFORIDUA", "WA", "BOLGATANGA"),
+address varchar(30),
+description varchar(200),
+color varchar(20),
+primary key(game_id),
+foreign key (sports_id) references sports(sports_id),
+foreign key (image_id) references seller_product (image_id));
 
-CREATE TABLE `products` (
-  `product_id` int(11) NOT NULL,
-  `product_cat` int(11) NOT NULL,
-  `product_brand` int(11) NOT NULL,
-  `product_title` varchar(200) NOT NULL,
-  `product_price` double NOT NULL,
-  `product_desc` varchar(500) DEFAULT NULL,
-  `product_image` varchar(100) DEFAULT NULL,
-  `product_keywords` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table sport_basketball(
+basketball_id int not null auto_increment,
+gender enum ("Male", "Female", "Unisex"),
+size enum ("Small","Medium", "Large"),
+sports_id int not null,
+Type enum ("Shoes", "Top", "Pant", "Short", "Full-kit", "Socks","Balls"),
+brand varchar (30),
+image1 varchar(255),
+image2 varchar(255),
+image3 varchar(255),
+image4 varchar(255),
+title varchar (20),
+image_id int not null,
+minimum_bid_price int not null,
+buy_price int not null,
+conditions enum ("Brand New", "Ghanaian Used", "Foreign Used"),
+location enum("ACCRA", "KUMASI", "CAPE COAST", "TAKORADI", "TEMALE", "UPPER EAST", "UPPER WEST", "VOLTA REGION", "KOFORIDUA", "WA", "BOLGATANGA"),
+address varchar(30),
+description varchar(200),
+color varchar(20),
+primary key(basketball_id),
+foreign key (sports_id) references sports(sports_id),
+foreign key (image_id) references seller_product (image_id));
 
---
--- Indexes for dumped tables
---
+create table sport_training_wear(
+training_id int not null auto_increment,
+gender enum ("Male", "Female", "Unisex"),
+size enum ("Small","Medium", "Large"),
+sports_id int not null,
+Type enum ("Shoes", "Top", "Pant", "Short", "Full-kit", "Socks","Balls"),
+brand varchar (30),
+image1 varchar(255),
+image2 varchar(255),
+image3 varchar(255),
+image4 varchar(255),
+image_id int not null,
+minimum_bid_price int not null,
+title varchar (20),
+buy_price int not null,
+conditions enum ("Brand New", "Ghanaian Used", "Foreign Used"),
+location enum("ACCRA", "KUMASI", "CAPE COAST", "TAKORADI", "TEMALE", "UPPER EAST", "UPPER WEST", "VOLTA REGION", "KOFORIDUA", "WA", "BOLGATANGA"),
+address varchar(30),
+description varchar(200),
+color varchar(20),
+primary key(training_id),
+foreign key (sports_id) references sports(sports_id),
+foreign key (image_id) references seller_product (image_id));
 
---
--- Indexes for table `brands`
---
-ALTER TABLE `brands`
-  ADD PRIMARY KEY (`brand_id`);
 
---
--- Indexes for table `cart`
---
-ALTER TABLE `cart`
-  ADD KEY `p_id` (`p_id`),
-  ADD KEY `c_id` (`c_id`);
+Create table houses(
+house_id int not null auto_increment,
+product_id int not null,
+bedroom_num int (30),
+bathroom_num int (30),
+facilities varchar (150),
+image1 varchar(255),
+image2 varchar(255),
+image3 varchar(255),
+image4 varchar(255),
+image_id int not null,
+house_type varchar(25),
+minimum_bid_price int not null,
+buy_price int not null,
+conditions varchar(30),
+title varchar (20),
+location varchar(30),
+house_size varchar(30),
+house_address varchar(30),
+color varchar(20),
+description varchar(200),
+primary key(house_id),
+foreign key (product_id) references sports(product_id),
+foreign key (image_id) references seller_product (image_id));
 
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`cat_id`);
+Create table arts(
+art_id int not null auto_increment,
+product_id int not null,
+image1 varchar(255),
+image2 varchar(255),
+image3 varchar(255),
+image4 varchar(255),
+image_id int not null,
+art_type varchar(25),
+minimum_bid_price int not null,
+buy_price int not null,
+conditions varchar(30),
+title varchar (20),
+location varchar(30),
+size varchar(30),
+address varchar(30),
+color varchar(20),
+description varchar(200),
+primary key(art_id),
+foreign key (product_id) references seller_product(product_id),
+foreign key (image_id) references seller_product (image_id));
 
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`customer_id`),
-  ADD UNIQUE KEY `customer_email` (`customer_email`);
+Create table watches(
+watch_id int not null auto_increment,
+product_id int not null,
+gender varchar(30),
+brand varchar(30),
+category varchar(30),
+facilities varchar (150),
+image1 varchar(255),
+image2 varchar(255),
+image3 varchar(255),
+image4 varchar(255),
+title varchar (20),
+image_id int not null,
+material_type varchar(25),
+minimum_bid_price int not null,
+buy_price int not null,
+conditions varchar(30),
+location varchar(30),
+color varchar(20),
+house_address varchar(30),
+description varchar(200),
+primary key(watch_id),
+foreign key (product_id) references seller_product(product_id),
+foreign key (image_id) references seller_product (image_id));
 
---
--- Indexes for table `orderdetails`
---
-ALTER TABLE `orderdetails`
-  ADD KEY `order_id` (`order_id`),
-  ADD KEY `product_id` (`product_id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `customer_id` (`customer_id`);
-
---
--- Indexes for table `payment`
---
-ALTER TABLE `payment`
-  ADD PRIMARY KEY (`pay_id`),
-  ADD KEY `customer_id` (`customer_id`),
-  ADD KEY `order_id` (`order_id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`product_id`),
-  ADD KEY `product_cat` (`product_cat`),
-  ADD KEY `product_brand` (`product_brand`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `brands`
---
-ALTER TABLE `brands`
-  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `customer`
---
-ALTER TABLE `customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `payment`
---
-ALTER TABLE `payment`
-  MODIFY `pay_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `cart`
---
-ALTER TABLE `cart`
-  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`p_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`c_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `orderdetails`
---
-ALTER TABLE `orderdetails`
-  ADD CONSTRAINT `orderdetails_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
-  ADD CONSTRAINT `orderdetails_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`);
-
---
--- Constraints for table `payment`
---
-ALTER TABLE `payment`
-  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
-  ADD CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
-
---
--- Constraints for table `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`product_cat`) REFERENCES `categories` (`cat_id`),
-  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`product_brand`) REFERENCES `brands` (`brand_id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+create table bidding(
+bid_count int not null auto_increment,
+customer_id int not null,
+item_id int not null,
+min_bid_price int not null,
+product_name varchar(50) not null,
+buy_price int not null,
+days datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+primary key(bid_count));
